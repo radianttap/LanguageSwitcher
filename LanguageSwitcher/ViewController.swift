613dd26e84e8446246f6881d97c9bcf2553f8e22
Aleksandar Vacic <av@radianttap.com>
2017-04-14 16:11:40 +0200

    Initial Commit

---
+++ LanguageSwitcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LanguageSwitcher
+//
+//  Created by Aleksandar Vacić on 14.4.17..
+//  Copyright © 2017. Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
