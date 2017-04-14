c7d6e64b2a14453f32b2690a7ae9784f3237029b
Aleksandar Vacic <av@radianttap.com>
2017-04-14 20:53:09 +0200

    Implemented initial setup, outlets and actions, keyboard handling Implemented conversion flow for entered numbers

---
+++ LanguageSwitcher/Formatters.swift
@@ -0,0 +1,32 @@
+//
+//  Formatters.swift
+//  LanguageSwitcher
+//
+//  Created by Aleksandar Vacić on 14.4.17..
+//  Copyright © 2017. Radiant Tap. All rights reserved.
+//
+
+import Foundation
+
+
+extension NumberFormatter {
+
+	static let moneyFormatter: NumberFormatter = {
+		let nf = NumberFormatter()
+		nf.generatesDecimalNumbers = true
+		nf.maximumFractionDigits = 2
+		nf.minimumFractionDigits = 2
+		nf.numberStyle = .decimal
+		return nf
+	}()
+}
+
+
+extension DateFormatter {
+
+	static let dobFormatter: DateFormatter = {
+		let df = DateFormatter()
+		df.dateStyle = .full
+		return df
+	}()
+}
--- LanguageSwitcher/ViewController.swift
+++ LanguageSwitcher/ViewController.swift
@@ -8,18 +8,138 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+	@IBOutlet fileprivate weak var gbBtn: UIButton!
+	@IBOutlet fileprivate weak var usBtn: UIButton!
+	@IBOutlet fileprivate weak var frBtn: UIButton!
+	@IBOutlet fileprivate weak var deBtn: UIButton!
+	@IBOutlet fileprivate weak var rsBtn: UIButton!
+	@IBOutlet fileprivate weak var ilBtn: UIButton!
+
+	fileprivate weak var currentBtn: UIButton!
+
+	@IBOutlet fileprivate weak var dateLabel: UILabel!
+	@IBOutlet fileprivate weak var textField: UITextField!
+	@IBOutlet fileprivate weak var convertedLabel: UILabel!
+
+	@IBOutlet fileprivate var doneButton: UIButton!
+	fileprivate var doneButtonBottomConstraint: NSLayoutConstraint?
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		setupUI()
+		setupNotifications()
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func dismissKeyboard(_ sender: Any) {
+		textField.resignFirstResponder()
 	}
-
-
 }
 
+fileprivate extension ViewController {
+	func setupNotifications() {
+		let nc = NotificationCenter.default
+		nc.addObserver(forName: NSNotification.Name.UIKeyboardWillShow, object: nil, queue: OperationQueue.main) {
+			[weak self] notification in
+			guard let `self` = self else { return }
+
+			guard
+				let userInfo = notification.userInfo,
+				let frameValue = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue
+			else {
+				return
+			}
+
+			let frame = frameValue.cgRectValue
+			if let lc = self.doneButtonBottomConstraint {
+				lc.constant = -frame.size.height
+			} else {
+				self.doneButtonBottomConstraint = self.doneButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -frame.size.height)
+				self.doneButtonBottomConstraint?.isActive = true
+			}
+			self.doneButton.alpha = 1
+		}
+	}
+
+	func setupUI() {
+		doneButton.alpha = 0
+		doneButton.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(doneButton)
+		self.doneButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+
+		convertedLabel.text = nil
+		textField.placeholder = NumberFormatter.moneyFormatter.string(for: 0)
+		let now = Date()
+		dateLabel.text = DateFormatter.dobFormatter.string(from: now)
+
+		guard
+			let languageCode = Locale.current.languageCode,
+			let countryCode = Locale.current.regionCode?.lowercased()
+		else { return }
+
+		switch countryCode {
+		case "gb":
+			currentBtn = gbBtn
+		case "us":
+			currentBtn = usBtn
+		case "fr":
+			currentBtn = frBtn
+		case "de":
+			currentBtn = deBtn
+		case "rs":	//	Serbia
+			currentBtn = rsBtn
+		case "il":	//	Israel
+			currentBtn = ilBtn
+
+		default:
+			switch languageCode {
+			case "en":
+				currentBtn = gbBtn
+			case "fr":
+				currentBtn = frBtn
+			case "de":
+				currentBtn = deBtn
+			case "sr":	//	Serbian
+				currentBtn = rsBtn
+			case "he":	//	Hebrew
+				currentBtn = ilBtn
+
+			default:
+				print("Unsupported language and/or country, sorry!")
+				return
+			}
+		}
+
+		currentBtn.backgroundColor = .white
+	}
+
+	func convert(_ str: String) {
+		guard let num = NumberFormatter.moneyFormatter.number(from: str)?.decimalValue else {
+			convertedLabel.text = nil
+			return
+		}
+		convertedLabel.text = NumberFormatter.moneyFormatter.string(for: num)
+	}
+}
+
+
+extension ViewController: UITextFieldDelegate {
+
+	func textFieldDidBeginEditing(_ textField: UITextField) {
+
+	}
+
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		self.doneButton.alpha = 0
+	}
+
+	@IBAction func textFieldDidChangeValue(_ textField: UITextField) {
+		guard let str = textField.text else {
+			convertedLabel.text = nil
+			return
+		}
+		convert(str)
+	}
+}
